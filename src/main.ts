b8002cd90bdffc42fd2b402962026234ad4addb3
Jakob Hellermann <jakob.hellermann@protonmail.com>
2021-01-05 13:54:52 +0100

    feat: set and clear status on webhook events
    Co-authored-by: Daniel Knapp <knapp@sipgate.de>
    Co-authored-by: Julian Bokelmann <bokelmann@sipgate.de>
    Co-authored-by: Michel Schwarz <mschwarz@sipgate.de>

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,22 @@
+import { createWebhookModule } from "sipgateio";
+import { readFileSync } from "fs";
+
 import { getStatus, setStatus, clearStatus } from "./slack";
+
+const webhookModule = createWebhookModule();
+
+const webhookServerPort = process.env.SIPGATE_WEBHOOK_SERVER_PORT || 8080;
+const webhookServerAddress = process.env.SIPGATE_WEBHOOK_SERVER_ADDRESS;
+if (!webhookServerAddress) {
+    throw new Error("SIPGATE_WEBHOOK_SERVER_ADDRESS environment variable not set");
+}
+
+interface SlackUserInfo {
+    slackMemberId: string;
+}
+
+
+const mappings: Record<string, SlackUserInfo | undefined> = JSON.parse(readFileSync("mappings.json", "utf8"));
 
 async function run(slackMemberId: string) {
     await setStatus(slackMemberId, {
@@ -7,6 +25,31 @@ async function run(slackMemberId: string) {
     });
 }
 
-const SLACK_MEMBER_ID = "...";
+webhookModule.createServer({ port: webhookServerPort, serverAddress: webhookServerAddress }).then(server => {
+    console.log("Listening on port", webhookServerPort)
 
-run(SLACK_MEMBER_ID).catch(console.error);
+    server.onAnswer(answerEvent => {
+        const relevantNumber = answerEvent.direction === "out" ? answerEvent.from : answerEvent.to;
+        const slackUserInfo: SlackUserInfo | undefined = mappings[relevantNumber];
+
+        if (!slackUserInfo) {
+            console.warn(`No slack user mapped for number ${relevantNumber}`);
+            return;
+        }
+
+        // TODO remember old status
+
+        setStatus(slackUserInfo.slackMemberId, {
+            status_emoji: ":phone:",
+            status_text: "Currently in a call",
+        }).catch(console.error);
+    });
+    server.onHangUp(hangupEvent => {
+        const relevantNumber = hangupEvent.direction === "out" ? hangupEvent.from : hangupEvent.to;
+        const slackUserInfo: SlackUserInfo | undefined = mappings[relevantNumber];
+        if (!slackUserInfo) return;
+
+        // TODO restore status
+        clearStatus(slackUserInfo.slackMemberId).catch(console.error);
+    });
+})
